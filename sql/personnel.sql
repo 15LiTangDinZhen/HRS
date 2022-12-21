a0b27260b807e4993fff80c24c6593182deeaf07
15LiTangDinZhen <109832388+15LiTangDinZhen@users.noreply.github.com>
2022-12-21 21:41:54 +0800

    Update personnel.sql

--- sql/personnel.sql
+++ sql/personnel.sql
@@ -1,124 +1,154 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : aliyun
-Source Server Version : 50641
-Source Host           : 47.106.248.229:3306
-Source Database       : personnel
+ Source Server         : LyhZzz
+ Source Server Type    : MySQL
+ Source Server Version : 80030 (8.0.30)
+ Source Host           : localhost:3306
+ Source Schema         : personnel
 
-Target Server Type    : MYSQL
-Target Server Version : 50641
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80030 (8.0.30)
+ File Encoding         : 65001
 
-Date: 2019-04-19 17:08:44
+ Date: 21/12/2022 21:37:55
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for dept
 -- ----------------------------
 DROP TABLE IF EXISTS `dept`;
-CREATE TABLE `dept` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `remark` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+CREATE TABLE `dept`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `remark` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 32 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (31, '开发部', '。。。');
 
 -- ----------------------------
 -- Table structure for document
 -- ----------------------------
 DROP TABLE IF EXISTS `document`;
-CREATE TABLE `document` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) DEFAULT NULL,
-  `filename` varchar(300) DEFAULT NULL,
-  `remark` varchar(300) DEFAULT NULL,
-  `path` varchar(50) DEFAULT NULL,
-  `create_date` varchar(20) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+CREATE TABLE `document`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `filename` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `remark` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `path` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `create_date` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 24 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of document
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for employee
 -- ----------------------------
 DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `dept_id` int(11) DEFAULT NULL,
-  `job_id` int(11) DEFAULT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `card_id` varchar(16) DEFAULT NULL,
-  `cardId` varchar(20) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `phone` varchar(11) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `sex` varchar(11) DEFAULT NULL,
-  `party` varchar(10) DEFAULT NULL,
-  `education` varchar(10) DEFAULT NULL,
-  `remark` varchar(500) DEFAULT NULL,
-  `createDate` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+CREATE TABLE `employee`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dept_id` int NULL DEFAULT NULL,
+  `job_id` int NULL DEFAULT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `card_id` varchar(16) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `cardId` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `address` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `sex` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `party` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `education` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `remark` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `createDate` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of employee
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for job
 -- ----------------------------
 DROP TABLE IF EXISTS `job`;
-CREATE TABLE `job` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `dept_id` int(11) DEFAULT NULL COMMENT '部门id',
-  `name` varchar(50) DEFAULT NULL,
-  `remark` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+CREATE TABLE `job`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dept_id` int NULL DEFAULT NULL COMMENT '部门id',
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `remark` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 21 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of job
+-- ----------------------------
+INSERT INTO `job` VALUES (20, 31, 'java工程师', '。。');
 
 -- ----------------------------
 -- Table structure for notice
 -- ----------------------------
 DROP TABLE IF EXISTS `notice`;
-CREATE TABLE `notice` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) DEFAULT NULL,
-  `content` text,
-  `createDate` char(20) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+CREATE TABLE `notice`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `createDate` char(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of notice
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for recruit
 -- ----------------------------
 DROP TABLE IF EXISTS `recruit`;
-CREATE TABLE `recruit` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `dept_id` int(11) DEFAULT NULL,
-  `job_id` int(11) DEFAULT NULL,
-  `person_num` int(11) DEFAULT NULL,
-  `remark` text CHARACTER SET utf8,
-  `status` varchar(255) DEFAULT NULL COMMENT '审核状态',
-  `create_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+CREATE TABLE `recruit`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dept_id` int NULL DEFAULT NULL,
+  `job_id` int NULL DEFAULT NULL,
+  `person_num` int NULL DEFAULT NULL,
+  `remark` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '审核状态',
+  `create_date` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of recruit
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `loginname` varchar(20) DEFAULT NULL,
-  `password` char(32) DEFAULT NULL,
-  `level` varchar(4) DEFAULT NULL COMMENT '级别',
-  `createdate` char(50) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `loginname` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `password` char(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `level` varchar(4) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '级别',
+  `createdate` char(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `username` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (9, 'root', 'e10adc3949ba59abbe56e057f20f883e', '0', '19-05-17 02:04:50', '夏祥');
+INSERT INTO `user` VALUES (11, '李友衡', 'e10adc3949ba59abbe56e057f20f883e', '0', '22-12-21 09:15:06', 'lyhzzz');
 
-
-INSERT INTO `personnel`.`user`(`id`, `loginname`, `password`, `level`, `createdate`, `username`) VALUES (9, 'root', '4a7d1ed414474e4033ac29ccb8653d9b', '0', '19-05-17 02:04:50', '金国民');
-INSERT INTO `personnel`.`user`(`id`, `loginname`, `password`, `level`, `createdate`, `username`) VALUES (10, 'huangjie', '4a7d1ed414474e4033ac29ccb8653d9b', '1', '19-05-17 02:13:13', '黄杰');
-
+SET FOREIGN_KEY_CHECKS = 1;
